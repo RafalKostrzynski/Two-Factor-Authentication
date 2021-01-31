1cb4ba1dd45698f12c47356a549df03e50258c31
RafalKostrzynski <53615746+RafalKostrzynski@users.noreply.github.com>
2021-01-31 14:44:06 +0100

    UserDetailService impl

---
+++ server/src/main/java/pl/kostrzynski/tfa/config/webSecurity/UserDetailServiceImpl.java
@@ -0,0 +1,26 @@
+package pl.kostrzynski.tfa.config.webSecurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pl.kostrzynski.tfa.repository.UserRepository;
+
+@Service
+@Primary
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+}
