e35820a7ef049d202bd7543e6e994094cbef010a
RafalKostrzynski <53615746+RafalKostrzynski@users.noreply.github.com>
2021-01-31 14:44:39 +0100

    SpringSecurity initial configuration

---
+++ server/src/main/java/pl/kostrzynski/tfa/config/webSecurity/WebSecurityConfig.java
@@ -0,0 +1,61 @@
+package pl.kostrzynski.tfa.config.webSecurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+    private final DataSource dataSource;
+
+    @Autowired
+    public WebSecurityConfig(UserDetailsService userDetailsService, DataSource dataSource) {
+        this.userDetailsService = userDetailsService;
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/for-user").authenticated()
+                .antMatchers("signup").permitAll()
+                .and()
+                .formLogin().loginPage("/login").defaultSuccessUrl("/for-user").permitAll()
+                .and()
+                .rememberMe().tokenRepository(persistentTokenRepository())
+                .and()
+                .logout().logoutSuccessUrl("/for-all");
+
+        // TODO delete this (its only purpose is to allow easier manual testing)
+        http.csrf().disable();
+        http.headers().disable();
+    }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        return tokenRepository;
+    }
+}
