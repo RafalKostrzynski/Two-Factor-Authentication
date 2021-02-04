b97419a1ecbfdfb99b7873b5c2e104c87f5c4824
RafalKostrzynski <53615746+RafalKostrzynski@users.noreply.github.com>
2021-02-04 20:51:27 +0100

    request storage_write and first try of storing keys

--- mobile/app/src/main/java/pl/kostrzynski/twofactorauthentication/FullscreenActivity.java
+++ mobile/app/src/main/java/pl/kostrzynski/twofactorauthentication/FullscreenActivity.java
@@ -3,11 +3,11 @@ package pl.kostrzynski.twofactorauthentication;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.*;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,6 +20,11 @@ import android.os.Handler;
 import android.view.View;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.security.*;
+import java.security.interfaces.ECPrivateKey;
+import java.security.interfaces.ECPublicKey;
+import java.security.spec.ECGenParameterSpec;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -32,7 +37,8 @@ public class FullscreenActivity extends AppCompatActivity {
      */
     private static final int UI_ANIMATION_DELAY = 300;
     private static final int READ_REQUEST_CODE = 42;
-    private static final int PERMISSION_REQUEST_STORAGE = 1000;
+    private static final int PERMISSION_REQUEST_STORAGE_READ = 1000;
+    private static final int PERMISSION_REQUEST_STORAGE_WRITE = 1001;
 
     private static final String SHARED_PREFS = "sharedPreferences";
     private static final String TEXT = "text";
@@ -79,7 +85,7 @@ public class FullscreenActivity extends AppCompatActivity {
     private void loadPK() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
                 checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_STORAGE);
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_STORAGE_READ);
 
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
@@ -88,7 +94,50 @@ public class FullscreenActivity extends AppCompatActivity {
     }
 
     private void generateECC() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE))
+                Toast.makeText(this, "Write permission is needed to save your keys", Toast.LENGTH_SHORT).show();
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_STORAGE_WRITE);
+        }
+        generateAndWriteKeysToStorage();
+        Toast.makeText(this, "Saved in Documents!", Toast.LENGTH_SHORT).show();
+    }
 
+    private void generateAndWriteKeysToStorage() {
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    setPriority(Thread.MAX_PRIORITY);
+
+                    KeyPairGenerator generator = KeyPairGenerator.getInstance("EC");
+                    generator.initialize(new ECGenParameterSpec("secp521r1"));
+                    KeyPair keyPair = generator.generateKeyPair();
+                    ECPublicKey publicKey = (ECPublicKey) keyPair.getPublic();
+                    ECPrivateKey privateKey = (ECPrivateKey) keyPair.getPrivate();
+
+                    Context context = FullscreenActivity.this;
+                    File path = context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS);
+
+
+                    // NOT LIKE THIS
+                    // TODO change the file format
+                    File privateKeyFile = new File(path, "private.key");
+                    File publicKeyFile = new File(path, "public.pub");
+
+                    try (FileOutputStream privateKeyOutput = new FileOutputStream(privateKeyFile);
+                         FileOutputStream publicKeyOutput = new FileOutputStream(publicKeyFile)) {
+                        privateKeyOutput.write(privateKey.getEncoded());
+                        publicKeyOutput.write(publicKey.getEncoded());
+                    }
+                    setAndSavePathTextView(path.getPath());
+                } catch (Exception e) {
+                    System.err.println("EC Exception\n" + e.toString());
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
     }
 
     private void readFileForPK(String path) {
@@ -121,6 +170,11 @@ public class FullscreenActivity extends AppCompatActivity {
                 path.substring(path.lastIndexOf("/") + 1, path.lastIndexOf(".")) : "";
     }
 
+    private void setAndSavePathTextView(String path) {
+        privateKeyName.setText(findFileNameFromString(path));
+        savePathToSharedPreferences(path);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
@@ -128,9 +182,7 @@ public class FullscreenActivity extends AppCompatActivity {
                 Uri uri = data.getData();
                 String path = uri.getPath();
                 path = path.substring(path.indexOf(":") + 1);
-                privateKeyName.setText(findFileNameFromString(path));
-
-                savePathToSharedPreferences(path);
+                setAndSavePathTextView(path);
                 Toast.makeText(this, "Key-path has been stored", Toast.LENGTH_SHORT).show();
             }
         }
@@ -138,7 +190,14 @@ public class FullscreenActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == PERMISSION_REQUEST_STORAGE) {
+        if (requestCode == PERMISSION_REQUEST_STORAGE_READ) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission not granted!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        } else if (requestCode == PERMISSION_REQUEST_STORAGE_WRITE) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show();
             } else {
