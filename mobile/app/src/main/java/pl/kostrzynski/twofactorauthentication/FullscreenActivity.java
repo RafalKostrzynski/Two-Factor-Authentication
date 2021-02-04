8af143582a384dd80320a111f7e2b44573aabbac
RafalKostrzynski <53615746+RafalKostrzynski@users.noreply.github.com>
2021-02-04 13:20:39 +0100

    stored key path to preferences

--- mobile/app/src/main/java/pl/kostrzynski/twofactorauthentication/FullscreenActivity.java
+++ mobile/app/src/main/java/pl/kostrzynski/twofactorauthentication/FullscreenActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
@@ -19,7 +20,6 @@ import android.os.Handler;
 import android.view.View;
 
 import java.io.File;
-import java.io.FileInputStream;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -34,6 +34,9 @@ public class FullscreenActivity extends AppCompatActivity {
     private static final int READ_REQUEST_CODE = 42;
     private static final int PERMISSION_REQUEST_STORAGE = 1000;
 
+    private static final String SHARED_PREFS = "sharedPreferences";
+    private static final String TEXT = "text";
+
     private View mContentView;
     private TextView privateKeyName;
 
@@ -45,19 +48,28 @@ public class FullscreenActivity extends AppCompatActivity {
         mContentView = findViewById(R.id.fullscreen_content);
 
         privateKeyName = findViewById(R.id.privateKeyName);
-        final Button loadPK = findViewById(R.id.read_PK_button);
-        final Button scanQR = findViewById(R.id.scan_code_button);
+        final Button loadPKButton = findViewById(R.id.read_PK_button);
+        final Button scanQRButton = findViewById(R.id.scan_code_button);
+        final Button generateECCButton = findViewById(R.id.generate_ECC_button);
 
-        scanQR.setOnClickListener(new View.OnClickListener() {
+        scanQRButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 scanQR();
             }
         });
-        loadPK.setOnClickListener(new View.OnClickListener() {
+        loadPKButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 loadPK();
             }
         });
+        generateECCButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                generateECC();
+            }
+        });
+
+        String path = loadPathFromPreferences();
+        privateKeyName.setText(findFileNameFromString(path));
     }
 
     private void scanQR() {
@@ -69,17 +81,44 @@ public class FullscreenActivity extends AppCompatActivity {
                 checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
             requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_STORAGE);
 
-
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("*/*");
         startActivityForResult(intent, READ_REQUEST_CODE);
     }
 
-    private void setPK(String path) {
-        File file = new File(path);
-        Toast.makeText(this, "Remember we do not store your Private Key but the path", Toast.LENGTH_SHORT).show();
-        privateKeyName.setText(file.getName());
+    private void generateECC() {
+
+    }
+
+    private void readFileForPK(String path) {
+        try {
+            File file = new File(path);
+
+            // TODO set key
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong, Please try again!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private String loadPathFromPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        String path = sharedPreferences.getString(TEXT, "");
+        if (new File(path).exists())
+            return path;
+        return "";
+    }
+
+    private void savePathToSharedPreferences(String path) {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(TEXT, path);
+        editor.apply();
+    }
+
+    private String findFileNameFromString(String path) {
+        return path.contains("/") ?
+                path.substring(path.lastIndexOf("/") + 1, path.lastIndexOf(".")) : "";
     }
 
     @Override
@@ -89,9 +128,10 @@ public class FullscreenActivity extends AppCompatActivity {
                 Uri uri = data.getData();
                 String path = uri.getPath();
                 path = path.substring(path.indexOf(":") + 1);
-                Toast.makeText(this, "" + path, Toast.LENGTH_SHORT).show();
+                privateKeyName.setText(findFileNameFromString(path));
 
-                setPK(path);
+                savePathToSharedPreferences(path);
+                Toast.makeText(this, "Key-path has been stored", Toast.LENGTH_SHORT).show();
             }
         }
     }
