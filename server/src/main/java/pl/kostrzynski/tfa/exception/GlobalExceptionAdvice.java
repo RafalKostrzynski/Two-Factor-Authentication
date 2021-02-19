ad8d13f16c3c252bd5a3cce78df151d904188a70
RafalKostrzynski <53615746+RafalKostrzynski@users.noreply.github.com>
2021-02-19 14:33:21 +0100

    Initial Exception handling

---
+++ server/src/main/java/pl/kostrzynski/tfa/exception/GlobalExceptionAdvice.java
@@ -0,0 +1,34 @@
+package pl.kostrzynski.tfa.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.mail.MessagingException;
+
+@ControllerAdvice
+public class GlobalExceptionAdvice {
+
+    @ExceptionHandler({MessagingException.class})
+    public final ResponseEntity<Object> exceptionHandler(Exception exception){
+        HttpStatus httpStatus;
+        String error;
+        if(exception instanceof MessagingException){
+            httpStatus=HttpStatus.INTERNAL_SERVER_ERROR;
+            error = "Couldn't send requested Email";
+        }else{
+            httpStatus=HttpStatus.INTERNAL_SERVER_ERROR;
+            error = "Unknown error occurred please try again later";
+        }
+        String message = secureMessageOutput(exception.getLocalizedMessage());
+
+        return new ResponseEntity<>(new ApiError(httpStatus,message,error), httpStatus);
+    }
+
+    private String secureMessageOutput(String message){
+        return message.contains("pl.kostrzynski.tfa.model.")?
+                message.replace("pl.kostrzynski.tfa.model.",""):message;
+    }
+
+}
