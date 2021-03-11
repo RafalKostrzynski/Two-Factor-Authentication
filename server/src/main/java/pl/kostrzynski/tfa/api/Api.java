88783eb752b70f2b6153fe0acb7ec76c7bb4e44f
RafalKostrzynski <53615746+RafalKostrzynski@users.noreply.github.com>
2021-03-11 14:01:04 +0100

    Modified the /verify-email to throw custom exception when user wasn't verified

--- server/src/main/java/pl/kostrzynski/tfa/api/Api.java
+++ server/src/main/java/pl/kostrzynski/tfa/api/Api.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pl.kostrzynski.tfa.exception.ApiErrorCodeEnum;
+import pl.kostrzynski.tfa.exception.ApiMethodException;
 import pl.kostrzynski.tfa.model.SecondAuth;
 import pl.kostrzynski.tfa.model.User;
 import pl.kostrzynski.tfa.service.SecondAuthService;
@@ -23,14 +25,16 @@ public class Api {
     private final VerificationTokenService verificationTokenService;
 
     @Autowired
-    public Api(SecondAuthService secondAuthService, UserService userService, VerificationTokenService verificationTokenService) {
+    public Api(SecondAuthService secondAuthService, UserService userService,
+               VerificationTokenService verificationTokenService) {
         this.secondAuthService = secondAuthService;
         this.userService = userService;
         this.verificationTokenService = verificationTokenService;
     }
 
     @PostMapping("/user")
-    public ResponseEntity<HttpStatus> addUser(@RequestBody User user, HttpServletRequest httpServletRequest) throws MessagingException {
+    public ResponseEntity<HttpStatus> addUser(@RequestBody User user, HttpServletRequest httpServletRequest)
+            throws MessagingException {
         userService.addNewUser(user, httpServletRequest);
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
@@ -43,10 +47,13 @@ public class Api {
     }
 
     @GetMapping("/verify-email/{token}")
-    public ResponseEntity<String> verifyEmailAndGenerateTokenForNewPublicKey(@PathVariable String token, HttpServletRequest httpServletRequest) {
+    public ResponseEntity<String> verifyEmailAndGenerateTokenForNewPublicKey(@PathVariable String token,
+                                                                             HttpServletRequest httpServletRequest) {
         User user = userService.verifyToken(token, "verify-email");
         //TODO test if doesn't throw errors when wrong data
-        return user.isEmailVerified() ?
-                new ResponseEntity<>(verificationTokenService.createUUIDLink(user, "add-public", httpServletRequest), HttpStatus.ACCEPTED) : new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        if (user.isEmailVerified()) return new ResponseEntity<>(verificationTokenService.
+                createUUIDLink(user, "add-public", httpServletRequest), HttpStatus.ACCEPTED);
+
+        throw new ApiMethodException("Email could not be verified, try again", ApiErrorCodeEnum.NOT_ACCEPTABLE);
     }
 }
