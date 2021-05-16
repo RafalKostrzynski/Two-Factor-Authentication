b757f8c40b3d6fca7804d78c55a5ed3cd86b0fa0
RafalKostrzynski <53615746+RafalKostrzynski@users.noreply.github.com>
2021-05-16 22:58:48 +0200

    Reset-password shows expiration time

--- webUI/src/app/components/reset-password/reset-password.component.ts
+++ webUI/src/app/components/reset-password/reset-password.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HttpService } from 'src/app/services/http.service';
 
 @Component({
@@ -9,16 +9,45 @@ import { HttpService } from 'src/app/services/http.service';
 })
 export class ResetPasswordComponent implements OnInit {
 
-  constructor(private httpService: HttpService, private activatedRoute: ActivatedRoute) { }
+  constructor(private httpService: HttpService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   errorMessage?: string;
   generateKeyQR?: string;
+  interval: any;
+  expirationTime?: number;
+  expirationTimeSeconds?: string;
 
   ngOnInit(): void {
     var token: string = this.activatedRoute.snapshot.paramMap.get("token")!;
     this.httpService.resetPassword(token).subscribe(
-      response => this.generateKeyQR = JSON.stringify(response), 
+      response => {
+        var expirationTimeFromQrCode = Math.floor((new Date(response.expirationTime).getTime() - new Date().getTime()) / 1000);
+        this.startCountDown(expirationTimeFromQrCode);
+        this.generateKeyQR = JSON.stringify(response)
+      },
       errorMessage => this.errorMessage = <any>errorMessage
     );
   }
+
+  private startCountDown(seconds: number) {
+    var decrementValue = 100 / 900;
+    this.interval = setInterval(() => {
+      this.expirationTime = seconds * decrementValue;
+      this.expirationTimeSeconds = this.getTimeString(seconds);
+      seconds--;
+      if (seconds < 0) {
+        clearInterval(this.interval);
+        this.router.navigate(["/sign-in"]);
+      }
+    }, 1000);
+  }
+
+  private getTimeString(seconds: number): string {
+    return ('0' + Math.floor(seconds % 3600 / 60)).slice(-2) + ":" + ('0' + Math.floor(seconds % 3600 % 60)).slice(-2);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
+
 }
