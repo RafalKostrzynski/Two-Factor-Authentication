b47f035fad57af42c9cbbb899c7cf6880b197e29
RafalKostrzynski <53615746+RafalKostrzynski@users.noreply.github.com>
2021-05-16 14:32:49 +0200

    Finished change key functionality

--- webUI/src/app/components/settings-key/settings-key.component.ts
+++ webUI/src/app/components/settings-key/settings-key.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { QrCode } from 'src/app/models/qr-code';
 import { HttpService } from 'src/app/services/http.service';
 
 @Component({
@@ -11,9 +12,37 @@ export class SettingsKeyComponent {
   constructor(private httpService: HttpService) { }
 
   isQRRequested: boolean = false;
+  generateKeyQR?: string;
+  errorMessage?: string;
+  interval: any;
+  expirationTime?: number;
+  expirationTimeSeconds?: number;
 
   changeView() {
     this.isQRRequested = !this.isQRRequested;
+    this.httpService.generateNewKey().subscribe((data: QrCode) => {
+      var expirationTimeFromQrCode = Math.floor((new Date(data.expirationTime).getTime() - new Date().getTime()) / 1000);
+      this.startCountDown(expirationTimeFromQrCode);
+      this.generateKeyQR = JSON.stringify(data)
+    },
+      errorMessage => this.errorMessage = errorMessage)
   }
 
+  private startCountDown(seconds: number) {
+    var decrementValue = 100 / 60;
+    this.interval = setInterval(() => {
+      this.expirationTime = seconds * decrementValue;
+      this.expirationTimeSeconds = seconds;
+      seconds--;
+      if (seconds < 0) {
+        clearInterval(this.interval);
+        this.isQRRequested = false;
+      }
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    this.isQRRequested = false;
+    clearInterval(this.interval);
+  }
 }
